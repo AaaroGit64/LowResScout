616b354845247e9e04007ac792d7acf76b786d15
Your Name <you@example.com>
2022-11-10 19:30:51 +0200

    Ginger Ale

--- Spatula.gd
+++ Spatula.gd
@@ -5,7 +5,7 @@ var jump_length : float = 5.5
 var jump_height : float = 2.0
 
 onready var player = $PLAYER
-
+onready var Pivotcamera = $pivotcam
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -15,6 +15,9 @@ onready var player = $PLAYER
 func _ready():
 	player.SetUp_Jump(jump_height, jump_length, run_speed) # Replace with function body.
 
+func _physics_process(delta):
+	Pivotcamera.translation = player.translation
+	Pivotcamera.translation.y = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
